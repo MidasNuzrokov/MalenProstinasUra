288c1e85f096218757f972b0d0ed9f9535ac3e55
Korzun Dmitriy <Korzun.DV@phystech.edu>
2019-09-17 15:14:10 +0300

    Initial commit

---
+++ examples/Prosti nas, Ura.py
@@ -0,0 +1,23 @@
+from graph import *
+
+penColor("black")
+penSize(5)
+brushColor("black")
+rectangle(0, 0, 1000, 300)
+penColor(90, 35, 17)
+penSize(5)
+brushColor(90, 35, 17)
+rectangle(0, 600, 1000, 300)
+penColor(195,152,35)
+brushColor(195,152,35)
+circle(380, 150, 90)
+penColor('black')
+penSize(0)
+brushColor(195,195,195)
+polygon([(80,195), (103,145),
+         (126,195), (80, 195)])
+rectangle(90, 70, 116, 185)
+polygon([(90,70), (103,45),
+         (116,70), (90,70)])
+
+run()
